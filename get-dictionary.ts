8e6c8b9ec4492beb9d2bf831733132ebe1d9402d
Sophin420 <krystian.bielowka@wp.pl>
2023-03-08 20:13:17 +0100

    dictionary ts fix

--- get-dictionary.ts
+++ get-dictionary.ts
@@ -3,9 +3,9 @@ import type { Locale } from './i18n-config'
 
 // We enumerate all dictionaries here for better linting and typescript support
 // We also get the defalt import for cleaner types
-const dictionaries = {
+const dictionaries: {[key: string]: () => Promise<{ [key: string]: { [key: string]: string } }> } = {
   en: () => import('./dictionaries/en.json').then((module) => module.default),
   pl: () => import('./dictionaries/pl.json').then((module) => module.default),
-}
+};
 
 export const getDictionary = async (locale: Locale) => dictionaries[locale]()
